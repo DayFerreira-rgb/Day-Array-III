75bb2dffdb02b5d04bb1621ec89ead61d8f9a54b
DayFerreira-rgb <eudayanesilvaferreira@gmail.com>
2025-11-12 16:49:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+///A lista ARRAY - Vetores
+
+var nomes = ["Beatriz", "Anna", "Osmar", "Pedro", "Roberto","Samara", "Ivete", "Ayrton", "Eduardo", "Marcelo", "Giovanna", "Pedro", "Luan", "Otávio", "Filipe", "Paulo", "Pablo", "Clara", "Lucas","Isabel", "Fernanda","Rafael", "Willian", "Simone", "Michel", "Sandoval","Murilo", "Luana", "Hamilton", "Jungo", "Joana"]
+
+var escolhaNomes = parseInt(prompt("Digite um número de 0 a 15"))
+
+
+console.log(nomes[escolhaNomes])
+document.write( "Nome:" + nomes [escolhaNomes])
